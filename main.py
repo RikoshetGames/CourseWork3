26566763525456025bd7ebae5411bcdd4354a8b3
RikoshetGames <ilyalew@mail.ru>
2023-09-29 00:02:53 +0300

    Add read_file funcs

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.utils import read_file, filter_operations, sort_by_date, mask_card, mask_account
+
+def main():
+    read = read_file("operations.json")
+    filtered = filter_operations(read)
+    last = sort_by_date(filtered)
+    # print(last)
+
+
+
+if __name__ == '__main__':
+    main()
